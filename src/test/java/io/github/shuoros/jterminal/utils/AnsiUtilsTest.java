7d24f59db9186220813903e3f40a8e4e748a7fba
Soroush <s_shemshadi@ymail.com>
2021-11-13 13:51:22 +0330

    [UPDATE] TextEntity sorted ranges and full text coverage unit tests

--- src/test/java/io/github/shuoros/jterminal/utils/AnsiUtilsTest.java
+++ src/test/java/io/github/shuoros/jterminal/utils/AnsiUtilsTest.java
@@ -16,6 +16,43 @@ import io.github.shuoros.jterminal.util.TextEntity;
 public class AnsiUtilsTest {
 
 	@Test
+	public void generatedAnsiCodeMustInOrderOfTextEntitiesRanges() {
+		// Givven
+		String text = "test";
+		List<TextEntity> enitities = List.of(//
+				new TextEntity(2, 4, Color.RED), //
+				new TextEntity(0, 2, Color.ORANGE));
+
+		// When
+		String generatedAnsiCode = AnsiUtils.generateCode(text, enitities);
+
+		// Then
+		assertTrue(generatedAnsiCode.indexOf("te") < generatedAnsiCode.indexOf("st"));
+	}
+
+	@Test
+	public void generatedAnsiCodeMustHaveFullCoverageOfStringWithoutFullCoverageOfTextEntity() {
+		// Givven
+		String text = "test";
+		List<TextEntity> enitities = List.of(//
+				new TextEntity(0, 2, Color.ORANGE));
+
+		// When
+		String generatedAnsiCode = AnsiUtils.generateCode(text, enitities);
+
+		// Then
+		assertEquals(2, generatedAnsiCode.split(AnsiUtils.RESET.replaceAll("\\[", "\\\\[")).length); // Color just
+																										// assigned to
+																										// range of 0 to
+																										// 2. We must
+																										// asure that
+																										// range 2 to 4
+																										// is exist with
+																										// default
+																										// colotr too.
+	}
+
+	@Test
 	public void mustThrowExceptionIfDetectOverlapInEntities() {
 		// Givven
 		String text = "test";
